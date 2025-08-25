0d24104552fad7cfb6a8892acbef633e3d312667
LDS <ashuraj.srivastav01@gmail.com>
2025-08-25 16:34:58 +0530

    Script is ready, now I'm going to import csv file to test

--- csv-user-migrator/process-csv.php
+++ csv-user-migrator/process-csv.php
@@ -11,16 +11,21 @@ if (($handle = fopen($csvFile, 'r')) !== false) {
     while (($row = fgetcsv($handle)) !== false) {
         $data = array_combine($header, $row);
 
-        // Example CSV columns: user_login, user_email, first_name, last_name
-        $login      = trim($data['user_login']);
-        $email      = trim($data['user_email']);
-        $first_name = trim($data['first_name']);
-        $last_name  = trim($data['last_name']);
+        # Example CSV columns: 
+        // ID, user_login, user_pass, user_nicename, user_email, 
+        // user_url, user_registered, user_activation_key, user_status, display_name
 
-        // Skip empty emails
-        if (empty($email)) {
-            continue;
-        }
+        // Relevant wp_users columns:
+        $rainMaker_user_id = trim($data['id']) ?? '';
+        $login = trim($data['user_login']) ?? '';
+        // $user_pass = trim($data['user_pass']) ?? '';
+        $user_nicename = trim($data['user_nicename']) ?? '';
+        $email = trim($data['user_email']) ?? '';
+        $user_url = trim($data['user_url']) ?? '';
+        $user_registered = trim($data['user_registered']) ?? '';
+        $user_activation_key = trim($data['user_activation_key']) ?? '';
+        $user_status = trim($data['user_status']) ?? '';
+        $display_name = trim($data['display_name']) ?? '';
 
         // Check if user exists already
         $user_id = $wpdb->get_var(
@@ -32,14 +37,24 @@ if (($handle = fopen($csvFile, 'r')) !== false) {
             $wpdb->insert(
                 $wpdb->prefix . 'users',
                 [
-                    'user_login'   => $login,
+                    'user_login' => $login,
+                    // 'user_pass' => $user_pass,
                     'user_pass'    => wp_hash_password('Default@123'), // default password
-                    'user_email'   => $email,
-                    'display_name' => $first_name . ' ' . $last_name
+                    'user_nicename' => $user_nicename,
+                    'user_email' => $email,
+                    'user_url' => $user_url,
+                    'user_registered' => $user_registered,
+                    'user_activation_key' => $user_activation_key,
+                    'user_status' => $user_status,
+                    'display_name' => $display_name,
                 ]
             );
             $user_id = $wpdb->insert_id;
 
+            // Relevant wp_usermeta keys:
+            // -> wp_capabilities
+            // -> wp_user_level
+            
             // Insert meta keys (wp_capabilities + wp_user_level)
             update_user_meta($user_id, $wpdb->prefix.'capabilities', ['subscriber' => true]);
             update_user_meta($user_id, $wpdb->prefix.'user_level', 0);
